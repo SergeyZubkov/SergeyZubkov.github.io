cf3977e72863ccbf6ddcc3bf687e7963920ef7bf
SergeyZubkov <Coh1bAAAA@mail.ru>
2016-11-15 17:19:36 +0300

    add scroll-top btn

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,20 @@
 $(document).ready(function() {
 
-$('.skill-list li').waypoint({
-	handler: function(direction) {
-		var $elm = $(this.element);
-		var $percentage = $elm.find('.percentage');
-		var value = $percentage.attr('data-percentage');
-		$percentage.width(0).animate({
-			width: value + '%'
-		},2000)
-	},
-	offset: 'bottom-in-view'
-})
+	$('.skill-list li').waypoint({
+		handler: function(direction) {
+			var $elm = $(this.element);
+			var $percentage = $elm.find('.percentage');
+			var value = $percentage.attr('data-percentage');
+			$percentage.width(0).animate({
+				width: value + '%'
+			},2000)
+		},
+		offset: 'bottom-in-view'
+	})
 
+	$('.btn-top').on('click', function() {
+			$('body').animate({
+				scrollTop: 0
+			}, "1000", 'swing')
+	})
 });
